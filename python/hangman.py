aef5a63fa07c66c83d66f940c793ba576cd5b014
Alex7k <anon@example.com>
2026-05-20 11:24:17 +0200

    simplify hangman

--- python/hangman.py
+++ python/hangman.py
@@ -1,23 +1,20 @@
 import random
 
 wordlist = [ "apple", "banana", "burger", "equilibrium", "operator", "keyboard", "paragraph", "obituary", "technology", "optimal" ]
-word = random.choice(wordlist)
-
-temp = []
-for i in range(len(word)):
-  temp.append("_ ")
+word = list(random.choice(wordlist)) # e.g. banana
+temp = list("_"*len(word)) # e.g. ______
 
 guessedletters = []
 
 while True:
   print(" ".join(temp))
-  print("Guessed letters: "+", ".join(guessedletters))
+  print(f"Guessed letters: {" ".join(guessedletters)}")
   if temp == list(word):
     print(f"You won with {len(guessedletters)} tries!")
     exit()
   userinput = input("Guess a letter: ")
   if len(userinput) != 1:
-    print("Invalid letter!")
+    print("Only one letter please!")
     continue
   if userinput in guessedletters:
     print("You've already guessed that letter!")
