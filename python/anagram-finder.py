4f25966dde140a62194c23189e8ad6c495a2e3d6
Alex7k <51133872+Alex7k@users.noreply.github.com>
2026-06-02 15:00:45 +0200

    update anagram-finder.py
    strips spaces when checking now

--- python/anagram-finder.py
+++ python/anagram-finder.py
@@ -1,23 +1,30 @@
 dict = [
-  'apple',
-  'banana',
-  'cherry',
-  'date',
-  'earth',
-  'heart',
-  'rathe',
-  'fried',
-  'fired'
+    "apple",
+    "banana",
+    "cherry",
+    "date",
+    "earth",
+    "heart",
+    "rathe",
+    "fried",
+    "fired",
+    "listen",
+    "silent",
+    "dormitory",
+    "dirty room",
+    "debit card",
+    "bad credit",
 ]
 
 input_word = "fried"
 
 anagrams = []
 for word in dict:
-  if len(input_word) == len(word) and input_word != word:
-    print(word)
-    if sorted(list(input_word)) == sorted(list(word)):
-      anagrams.append(word)
+    normalized_input = input_word.replace(" ", "")
+    normalized_word = word.replace(" ", "")
+    if len(normalized_input) == len(normalized_word) and input_word != word:
+        if sorted(list(normalized_input)) == sorted(list(normalized_word)):
+            anagrams.append(word)
 
 print(f"Anagrams to '{input_word}':")
 print(anagrams)
