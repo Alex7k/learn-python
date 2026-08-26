3b9136bdb77e378738ea4185bd87daa78d9e8daf
Alex7k <51133872+Alex7k@users.noreply.github.com>
2026-08-26 13:22:41 +0200

    fix password-generator.py ruff violation

--- python/password-generator.py
+++ python/password-generator.py
@@ -5,6 +5,6 @@ symbols = list("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-=
 
 password = ""
 for i in range(length):
-  password += random.choice(symbols) # random.choice() selects random object from the array
+  password += random.choice(symbols)  # random.choice() selects random object from the array
 
 print("Password: " + password)
