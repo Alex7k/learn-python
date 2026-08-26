bc9d4429c0f65ea4b652ce9a45e2fb6d38dfbb85
Alex7k <51133872+Alex7k@users.noreply.github.com>
2026-08-26 13:21:21 +0200

    update collatzconjecture.py

--- python/collatzconjecture.py
+++ python/collatzconjecture.py
@@ -1,34 +1,32 @@
 # https://en.wikipedia.org/wiki/Collatz_conjecture
 
-# Even number: / 2
-# Odd number: * 3 + 1
+# Rules:
+# If number even: divide by 2
+# If number odd: multiply by 3 and add 1
 
 samples = 100000
 leaderboard = []
-for startingnumber in range(1, samples):
+for starting_number in range(1, samples):
   # measure how many steps to end up in the 4, 2, 1 loop
   step = 0
-  highestnumber = startingnumber
-  number = startingnumber
+  largest_number = starting_number
+  number = starting_number
   while True:
     step += 1
-    if number % 2 == 0:
+    if number % 2 == 0:  # even
       number = number // 2
-    elif number % 2 == 1:
+    else:  # odd
       number = number * 3 + 1
-    if number > highestnumber:
-      highestnumber = number
+    largest_number = max(largest_number, number)
     # print(number)
     if number == 1:
-      # print(f"{startingnumber} arrived at loop at step {step} with highest number being {highestnumber}")
-      leaderboard.append([startingnumber, step, highestnumber])
+      # print(f"{starting_number} arrived at loop at step {step} with highest number being {largest_number}")
+      leaderboard.append([starting_number, step, largest_number])
       break
 
 print("Leaderboard:")
 print("Starting number, achieved step, highest number")
 leaderboard.sort(key=lambda x: x[2], reverse=True)
-leaderboardlength = len(leaderboard)
-if 10 < leaderboardlength:
-  leaderboardlength = 10
-for i in range(leaderboardlength):
+leaderboard_length = min(len(leaderboard), 10)
+for i in range(leaderboard_length):
   print(leaderboard[i])
