ddb487eb6774909a455d1e668c20de9ae1d93599
Alex7k <51133872+Alex7k@users.noreply.github.com>
2026-08-26 13:21:39 +0200

    update hangman.py

--- python/hangman.py
+++ python/hangman.py
@@ -1,26 +1,40 @@
 import random
+import sys
 
-wordlist = [ "apple", "banana", "burger", "equilibrium", "operator", "keyboard", "paragraph", "obituary", "technology", "optimal" ]
-word = list(random.choice(wordlist)) # e.g. banana
-temp = list("_"*len(word)) # e.g. ______
+WORDLIST = [
+    "apple",
+    "banana",
+    "burger",
+    "equilibrium",
+    "operator",
+    "keyboard",
+    "paragraph",
+    "obituary",
+    "technology",
+    "optimal",
+]
+HEARTS = 5
+
+word = list(random.choice(WORDLIST))  # e.g. banana
+temp = list("_" * len(word))  # e.g. ______
 
 guessedletters = []
 
 while True:
-  print(" ".join(temp))
-  print(f"Guessed letters: {" ".join(guessedletters)}")
-  if temp == list(word):
-    print(f"You won with {len(guessedletters)} tries!")
-    exit()
-  userinput = input("Guess a letter: ")
-  if len(userinput) != 1:
-    print("Only one letter please!")
-    continue
-  if userinput in guessedletters:
-    print("You've already guessed that letter!")
-    continue
-  else:
-    guessedletters.append(userinput)
-    for i in range(len(word)):
-      if userinput == word[i]:
-        temp[i] = word[i]
+    print(" ".join(temp))  # display status (e.g. "b _ n _ n _")
+    print(f"Guessed letters: {', '.join(guessedletters)}")
+    if temp == list(word):
+        print(f"You won with {len(guessedletters)} tries!")
+        sys.exit()
+    userinput = input("Guess a letter: ").lower()
+    if len(userinput) != 1:
+        print("Only one letter please!")
+        continue
+    if userinput in guessedletters:
+        print("You've already guessed that letter!")
+        continue
+    else:
+        guessedletters.append(userinput)
+        for i in range(len(word)):
+            if userinput == word[i]:
+                temp[i] = word[i]
